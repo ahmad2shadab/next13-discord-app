588b5a6252f6595d45ba3ed4620728dbb7fb8e4f
ahmad2shadab <ahmad2shadab@gmail.com>
2024-03-09 00:04:15 +0530

    Created general button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <p className="text-3xl font-bold text-indigo-500">Hello Discord App</p>
+    <div>
+      <p className="text-3xl font-bold text-indigo-500">Hello Discord App</p>
+      <Button variant="outline">Button</Button>
+    </div>
   );
 }
